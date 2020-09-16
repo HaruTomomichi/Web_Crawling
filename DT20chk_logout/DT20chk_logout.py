55d83f70528462878ec7e55334bc177d6f87055f
ZINO <wlsrkwkak@naver.com>
2020-09-16 17:40:39 +0900

    Jang

--- DT20chk_logout/DT20chk_logout.py
+++ DT20chk_logout/DT20chk_logout.py
@@ -1,27 +1,29 @@
 import sys
+from time import sleep
 
-print("*"*50)
-print("공공데이터 청년 인턴십 자동 퇴근 시스템 v0.2")
-print("*"*50)
+print("*"*50 + "\n공공데이터 청년 인턴십 세무과 자동 퇴근 시스템 v0.2\n" + "*"*50)
 
 id = input('아이디를 입력해주세요 : ')
-password = input('패스워드를 입력해주세요 : ')
-content = input('오늘 업무 내용을 입력해주세요 (최대 50자) : ')
-name = input('성함을 입력해주세요 : ')
+password = input('\n패스워드를 입력해주세요 : ')
+content = input('\n오늘 업무 내용을 입력해주세요 (최대 50자) : ')
+name = input('\n성함을 입력해주세요 : ')
 content = content[:50]
-big_div = '3'
-small_div = '2'
+big_div,small_div = ['3','2']
 
-print("*"*50)
-print("id = ",id)
-print("password = ",password)
-print("업무 내용 = ",content)
-print("성함 = ",name)
-answer = input("입력하신 내용이 다음과 같습니까? (y/n) : ")
+print("*"*50 + "\nid = ",id)
+print("\npassword = ",password)
+print("\n업무 내용 = ",content)
+print("\n성함 = ",name)
+answer = input("\n입력하신 내용이 다음과 같습니까? (y/n) : ")
 
-
-if answer == 'n':
-    print("프로그램을 종료합니다 다시 실행시켜 주십시오")
+if answer == 'y':
+    print("*" * 50 + "\n업무 중분류 체계 - 공공데이터 보유현황 정리 및 메타데이터 등록")
+    print("\n업무 소분류 체계 - 기타")
+    print("\n가상 페이지를 생성합니다...")
+    sleep(2)
+else:
+    print("*" * 50 + "프로그램을 종료합니다 다시 실행시켜 주십시오")
+    sleep(2)
     sys.exit()
 
 # big_div = input('업무 중분류 체계 코드를 입력해주세요 (ex: 2) : ')
@@ -34,7 +36,6 @@ if answer == 'n':
 # print("현재는 코드 3로 고정되어 있습니다 (테스트 중)")
 
 from selenium import webdriver
-from time import sleep
 
 url = "http://dt20chk.hyosungitx.com/main"
 
@@ -60,24 +61,20 @@ except:
 driver.find_element_by_xpath('//*[@id="btnSubmit"]').click()
 sleep(2)
 
-# 중분류
-# //*[@id="selCategory1"]/option[2]
-# //*[@id="selCategory1"]/option[3]
-# //*[@id="selCategory1"]/option[4]
-# //*[@id="selCategory1"]/option[6]
-
-# 소분류
-sleep(2)
 driver.find_element_by_xpath('//*[@id="selCategory1"]/option['+ big_div +']').click()
 driver.find_element_by_xpath('//*[@id="selCategory2"]/option[4]').click()
 driver.find_element_by_xpath('//*[@id="taEtcMemo"]').send_keys(content)
-
 driver.find_element_by_xpath('//*[@id="btnSubmit"]').click()
 sleep(2)
 
 driver.find_element_by_xpath('//*[@id="iptUserName1"]').send_keys(name)
 driver.find_element_by_xpath('//*[@id="iptUserName2"]').send_keys(name)
-# driver.find_element_by_xpath('//*[@id="btnSubmit"]').click()
+driver.find_element_by_xpath('//*[@id="btnSubmit"]').click()
+sleep(2)
 
-sleep(3)
 driver.close()
+
+print("*" * 50 + "\n성공적으로 작업이 완료되었습니다")
+print("\n2초 뒤에 프로그램을 종료합니다")
+sleep(2)
+sys.exit()

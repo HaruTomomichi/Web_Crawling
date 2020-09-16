8bef2445008af6384189ab408619ee6c7af78a55
ZINO <wlsrkwkak@naver.com>
2020-09-16 14:50:37 +0900

    Jang

--- DT20chk_logout/DT20chk_logout.py
+++ DT20chk_logout/DT20chk_logout.py
@@ -1,21 +1,37 @@
+import sys
+
 print("*"*50)
-print("공공데이터 청년 인턴십 자동 퇴근 시스템 v0.1")
+print("공공데이터 청년 인턴십 자동 퇴근 시스템 v0.2")
 print("*"*50)
 
-print("업무 중분류 체계는 다음과 같습니다")
-print(" - 공공데이터포털 목록등록체계 및 메타정보 점검 : 2") # DR0010
-print(" - 공공데이터 보유현황 정리 및 메타데이터 등록 : 3") # DR0011
-print(" - 공공데이터 목록 구성 및 개방 : 4") # DR0012
-print(" - 기관자체 개방포털 및 개방데이터 정비 : 6") # DR0014
-print("현재는 코드 3로 고정되어 있습니다 (테스트 중)")
+id = input('아이디를 입력해주세요 : ')
+password = input('패스워드를 입력해주세요 : ')
+content = input('오늘 업무 내용을 입력해주세요 (최대 50자) : ')
+name = input('성함을 입력해주세요 : ')
+content = content[:50]
+big_div = '3'
+small_div = '2'
 
-# id = input('아이디를 입력해주세요 : ')
-# password = input('패스워드를 입력해주세요 : ')
-# big_div = input('업무 중분류 체계 코드를 입력해주세요 (ex: 2) : )
-# small_div = input('업무 소분류 체계 코드를 입력해주세요 (ex: 2) : )
-# content = input('오늘 업무 내용을 입력해주세요 (최대 50자) : ')
-# 슬라이싱 기능 포함
-# name = input('성함을 입력해주세요 : ')
+print("*"*50)
+print("id = ",id)
+print("password = ",password)
+print("업무 내용 = ",content)
+print("성함 = ",name)
+answer = input("입력하신 내용이 다음과 같습니까? (y/n) : ")
+
+
+if answer == 'n':
+    print("프로그램을 종료합니다 다시 실행시켜 주십시오")
+    sys.exit()
+
+# big_div = input('업무 중분류 체계 코드를 입력해주세요 (ex: 2) : ')
+# small_div = input('업무 소분류 체계 코드를 입력해주세요 (ex: 2) : ')
+# print("업무 중분류 체계는 다음과 같습니다")
+# print(" - 공공데이터포털 목록등록체계 및 메타정보 점검 : 2") # DR0010
+# print(" - 공공데이터 보유현황 정리 및 메타데이터 등록 : 3") # DR0011
+# print(" - 공공데이터 목록 구성 및 개방 : 4") # DR0012
+# print(" - 기관자체 개방포털 및 개방데이터 정비 : 6") # DR0014
+# print("현재는 코드 3로 고정되어 있습니다 (테스트 중)")
 
 from selenium import webdriver
 from time import sleep

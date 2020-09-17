34c990e90225d62c6141aa2f8b796cba6423d808
ZINO <wlsrkwkak@naver.com>
2020-09-17 11:08:23 +0900

    Jang

--- DT20chk_logout/DT20chk_logout.py
+++ DT20chk_logout/DT20chk_logout.py
@@ -3,19 +3,22 @@ import sys
 import schedule
 from time import sleep
 from selenium import webdriver
+from datetime import datetime
 
 class WEBSYSTEM:
-    driver = webdriver.Chrome(
-        executable_path="webdriver/chromedriver.exe"
-    )
 
     def __init__(self):
         self.ID = input('아이디를 입력해주세요 : ')
         self.password = input('\n패스워드를 입력해주세요 : ')
         self.content = input('\n오늘 업무 내용을 입력해주세요 (최대 50자) : ')[:50]
         self.name = input('\n성함을 입력해주세요 : ')
-        self.big_div, self.small_div = ['3','2']
+        self.big_div, self.small_div = ['3', '2']
+
+        WEBSYSTEM.one_more_chk(self)
         self.url = 'http://dt20chk.hyosungitx.com/main'
+        self.driver = webdriver.Chrome(
+            executable_path="webdriver/chromedriver.exe"
+        )
 
     def one_more_chk(self):
         print("*" * 50 + "\nid = ", self.ID)
@@ -41,31 +44,31 @@ class WEBSYSTEM:
         self.end_off_work()
 
     def login(self):
-        WEBSYSTEM.driver.find_element_by_xpath('//*[@id="iptUser_id"]').send_keys(self.ID)
-        WEBSYSTEM.driver.find_element_by_xpath('//*[@id="iptUser_pass"]').send_keys(self.password)
-        WEBSYSTEM.driver.find_element_by_xpath('//*[@id="btnSubmit"]').click()
+        self.driver.find_element_by_xpath('//*[@id="iptUser_id"]').send_keys(self.ID)
+        self.driver.find_element_by_xpath('//*[@id="iptUser_pass"]').send_keys(self.password)
+        self.driver.find_element_by_xpath('//*[@id="btnSubmit"]').click()
 
     def zero_off_work(self):
-        WEBSYSTEM.driver.get(self.url)
+        self.driver.get(self.url)
 
         try:
             WEBSYSTEM.login(self)
             sleep(2)
         except:
-            WEBSYSTEM.driver.find_element_by_xpath('/html/body/div[3]/button').click()
+            self.driver.find_element_by_xpath('/html/body/div[3]/button').click()
             sleep(2)
             WEBSYSTEM.login(self)
             sleep(2)
 
     def first_off_work(self):
-        WEBSYSTEM.driver.find_element_by_xpath('//*[@id="btnSubmit"]').click()
-        sleep(10000)
+        self.driver.find_element_by_xpath('//*[@id="btnSubmit"]').click()
+        sleep(2)
 
     def second_off_work(self):
-        WEBSYSTEM.driver.find_element_by_xpath('//*[@id="selCategory1"]/option[' + self.big_div + ']').click()
-        WEBSYSTEM.driver.find_element_by_xpath('//*[@id="selCategory2"]/option[4]').click()
-        WEBSYSTEM.driver.find_element_by_xpath('//*[@id="taEtcMemo"]').send_keys(self.content)
-        WEBSYSTEM.driver.find_element_by_xpath('//*[@id="btnSubmit"]').click()
+        self.driver.find_element_by_xpath('//*[@id="selCategory1"]/option[' + self.big_div + ']').click()
+        self.driver.find_element_by_xpath('//*[@id="selCategory2"]/option[4]').click()
+        self.driver.find_element_by_xpath('//*[@id="taEtcMemo"]').send_keys(self.content)
+        self.driver.find_element_by_xpath('//*[@id="btnSubmit"]').click()
         sleep(2)
 
     def third_off_work(self):
@@ -75,7 +78,7 @@ class WEBSYSTEM:
         sleep(2)
 
     def end_off_work(self):
-        WEBSYSTEM.driver.close()
+        self.driver.close()
 
         print("*" * 50 + "\n성공적으로 작업이 완료되었습니다")
         print("\n2초 뒤에 프로그램을 종료합니다")
@@ -83,12 +86,12 @@ class WEBSYSTEM:
         sys.exit()
 
 if __name__ == '__main__':
+    print("*" * 50 + "\n공공데이터 청년 인턴십 세무과 자동 퇴근 시스템 v0.3 (0.2 → 0.3)\n" + "*" * 50)
     person = WEBSYSTEM()
-    person.one_more_chk()
-
-    schedule.every().day.at('10:04').do(person.total_off_work())
+    schedule.every().day.at('18:00').do(person.total_off_work)
 
     while True:
+        print("\n시간을 체크하고 있습니다... 현재 시간 : ",datetime.now())
         schedule.run_pending()
         sleep(1)
 
